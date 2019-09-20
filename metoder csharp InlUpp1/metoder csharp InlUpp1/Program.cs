c5cf94d680508246d10d93ae23fb94f8f5d72110
Emad <emal02002@utb.vaxjo.se>
2019-09-20 09:01:38 +0200

    Hej

---
+++ metoder csharp InlUpp1/metoder csharp InlUpp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metoder_csharp_InlUpp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
