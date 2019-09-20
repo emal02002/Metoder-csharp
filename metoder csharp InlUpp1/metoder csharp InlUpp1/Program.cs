6841f72acb652406c01bbd60b531b3156d52e49d
Emad <emal02002@utb.vaxjo.se>
2019-09-20 09:48:23 +0200

    Har gjort klart 1 metod uppgiften

--- metoder csharp InlUpp1/metoder csharp InlUpp1/Program.cs
+++ metoder csharp InlUpp1/metoder csharp InlUpp1/Program.cs
@@ -6,7 +6,22 @@ namespace metoder_csharp_InlUpp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
         }
+
+        static double CirkelOmkrets(double r)
+        {
+            return (r + r * 3.14);
+        }
+        static double CirkelArea(double r)
+        {
+            return (2 * r * 3.14);
+        }
+
+
+
+
     }
 }
